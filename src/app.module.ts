ad0a440bb844d10df12a727a43e9084cb4989479
Zhang YingJie <jie736805161@qq.com>
2025-12-22 17:27:11 +0800

    feat: save

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { OrderModule } from './order/order.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env', // 指定环境文件路径
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql', // 数据库类型
+        entities: [], // 数据表实体，synchronize为true时，自动创建表，生产环境建议关闭
+        host: configService.get('DB_HOST'), // 主机，默认为localhost
+        port: configService.get<number>('DB_PORT'), // 端口号
+        username: configService.get('DB_USER'), // 用户名
+        password: configService.get('DB_PASSWORD'), // 密码
+        database: configService.get('DB_NAME'), //数据库名
+        driver: require('mysql2'),
+        timezone: '+08:00', //服务器上配置的时区
+        synchronize: true, //根据实体自动创建数据库表， 生产环境建议关闭
+      }),
     }),
     UserModule,
     OrderModule,
